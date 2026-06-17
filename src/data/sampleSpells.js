45c817196bf4e7a97ec6851fcb77255a068645e7
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-17 19:08:57 -0300

    feat: add sample spell data

---
+++ src/data/sampleSpells.js
@@ -0,0 +1,112 @@
+export const sampleSpells = [
+  {
+    name: "Fire Bolt",
+    level: "Cantrip",
+    school: "Evocation",
+    castingTime: "1 action",
+    range: "120 feet",
+    components: "V, S",
+    duration: "Instantaneous",
+    description:
+      "You hurl a mote of fire at a creature or object within range. Make a ranged spell attack. On a hit, the target takes fire damage.",
+  },
+  {
+    name: "Mage Armor",
+    level: "1st",
+    school: "Abjuration",
+    castingTime: "1 action",
+    range: "Touch",
+    components: "V, S, M",
+    duration: "8 hours",
+    description:
+      "You touch a willing creature who is not wearing armor. Until the spell ends, the target's base Armor Class becomes 13 plus its Dexterity modifier.",
+  },
+  {
+    name: "Shield",
+    level: "1st",
+    school: "Abjuration",
+    castingTime: "1 reaction",
+    range: "Self",
+    components: "V, S",
+    duration: "1 round",
+    description:
+      "An invisible barrier of magical force appears and protects you. Until the start of your next turn, you gain a bonus to AC and take no damage from magic missile.",
+  },
+  {
+    name: "Misty Step",
+    level: "2nd",
+    school: "Conjuration",
+    castingTime: "1 bonus action",
+    range: "Self",
+    components: "V",
+    duration: "Instantaneous",
+    description:
+      "Briefly surrounded by silvery mist, you teleport up to 30 feet to an unoccupied space that you can see.",
+  },
+  {
+    name: "Detect Magic",
+    level: "1st",
+    school: "Divination",
+    castingTime: "1 action",
+    range: "Self",
+    components: "V, S",
+    duration: "Concentration, up to 10 minutes",
+    description:
+      "For the duration, you sense the presence of magic within 30 feet of you. You can use your action to see a faint aura around visible creatures or objects that bear magic.",
+  },
+  {
+    name: "Sleep",
+    level: "1st",
+    school: "Enchantment",
+    castingTime: "1 action",
+    range: "90 feet",
+    components: "V, S, M",
+    duration: "1 minute",
+    description:
+      "This spell sends creatures into a magical slumber. Creatures within range are affected in ascending order of their current hit points.",
+  },
+  {
+    name: "Mirror Image",
+    level: "2nd",
+    school: "Illusion",
+    castingTime: "1 action",
+    range: "Self",
+    components: "V, S",
+    duration: "1 minute",
+    description:
+      "Three illusory duplicates of yourself appear in your space. Until the spell ends, the duplicates move with you and mimic your actions.",
+  },
+  {
+    name: "Ray of Frost",
+    level: "Cantrip",
+    school: "Evocation",
+    castingTime: "1 action",
+    range: "60 feet",
+    components: "V, S",
+    duration: "Instantaneous",
+    description:
+      "A frigid beam of blue-white light streaks toward a creature within range. On a hit, it takes cold damage and its speed is reduced until your next turn.",
+  },
+  {
+    name: "Feather Fall",
+    level: "1st",
+    school: "Transmutation",
+    castingTime: "1 reaction",
+    range: "60 feet",
+    components: "V, M",
+    duration: "1 minute",
+    description:
+      "Choose up to five falling creatures within range. A falling creature's rate of descent slows until the spell ends.",
+  },
+  {
+    name: "Thunderwave",
+    level: "1st",
+    school: "Evocation",
+    castingTime: "1 action",
+    range: "Self",
+    components: "V, S",
+    duration: "Instantaneous",
+    description:
+      "A wave of thunderous force sweeps out from you. Each creature in the area must endure the force or be pushed away from you.",
+  },
+];
