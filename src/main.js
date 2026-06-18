9834e25ec9ffadc5f5119115a5d5bafd69320f39
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-17 23:34:44 -0300

    feat: render print job inspector

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
+import { createPrintJobInspector } from "./app/createPrintJobInspector.js";
 import { createSpellCardsJob } from "./printJobs/spellCardsJob.js";
 import { renderPrintDocument } from "./render/renderPrintDocument.js";
 
+const inspectorTarget = document.querySelector("#printJobInspector");
 const previewTarget = document.querySelector("#printPreview");
 const printButton = document.querySelector("#printButton");
 
-const printDocument = createSpellCardsJob();
+const spellCardsJob = createSpellCardsJob();
 
-renderPrintDocument(printDocument, previewTarget);
+renderPrintDocument(spellCardsJob.printDocument, previewTarget);
+inspectorTarget.append(createPrintJobInspector(spellCardsJob));
 
 printButton.addEventListener("click", () => {
   window.print();
