62585bf3f045f74dfb226385f61ba336b2601bc8
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-17 23:52:36 -0300

    feat: apply spell card theme context in job

--- src/printJobs/spellCardsJob.js
+++ src/printJobs/spellCardsJob.js
@@ -1,12 +1,14 @@
 import { sampleSpells } from "../data/sampleSpells.js";
 import { createPrintDocument } from "../core/print/createPrintDocument.js";
 import { spellCardManifest } from "../templates/spellCard/manifest.js";
+import { createSpellCardTemplateContext } from "../templates/spellCard/resolveSpellCardTheme.js";
 import { spellCardTemplate } from "../templates/spellCard/template.js";
 import { spellCardStyles } from "../templates/spellCard/styles.js";
 
 export function createSpellCardsJob() {
+  const templateData = sampleSpells.map(createSpellCardTemplateContext);
   const printDocument = createPrintDocument({
-    data: sampleSpells,
+    data: templateData,
     manifest: spellCardManifest,
     templateHtml: spellCardTemplate,
     templateStyles: spellCardStyles,
@@ -15,7 +17,7 @@ export function createSpellCardsJob() {
   return {
     id: "spell-cards",
     name: "Spell Cards",
-    data: sampleSpells,
+    data: templateData,
     manifest: spellCardManifest,
     templateHtml: spellCardTemplate,
     templateStyles: spellCardStyles,
