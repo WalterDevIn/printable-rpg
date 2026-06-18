9830e753283cb6cb4a83c6a6ce922063d0902621
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-18 00:16:33 -0300

    feat: add flow spell card manifest variant

---
+++ src/templates/spellCard/flow/manifest.js
@@ -0,0 +1,32 @@
+import { A4_PORTRAIT } from "../../../core/layout/pageSizes.js";
+
+export const flowSpellCardManifest = {
+  id: "spellCardFlow",
+  family: "card",
+  input: "Spell",
+  mode: "repeatable-fixed",
+
+  size: {
+    widthMm: 63,
+    heightMm: 88,
+  },
+
+  layout: {
+    unit: "mm",
+    gridMm: null,
+  },
+
+  page: A4_PORTRAIT,
+
+  pagination: {
+    stackable: true,
+    composer: "grid-pack",
+    allowRotate: false,
+    marginMm: 10,
+    gapMm: 5,
+  },
+
+  overflow: {
+    strategy: "fail",
+  },
+};
