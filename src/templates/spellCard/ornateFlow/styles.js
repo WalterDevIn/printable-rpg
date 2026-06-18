aae553e0fd71a4ea75a9e406bf07377d2bfee686
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-18 14:45:33 -0300

    feat: add ornate flow spell card styles

---
+++ src/templates/spellCard/ornateFlow/styles.js
@@ -0,0 +1,10 @@
+export const ornateFlowSpellCardStyles = `
+.spell--ornate-flow .spell__content {
+  gap: 3mm;
+}
+
+.spell--ornate-flow .spell__caption {
+  font-size: 3.35mm;
+  line-height: 1.26;
+}
+`;
