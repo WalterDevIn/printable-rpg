4555d0077d341a74f481796fc168336606941557
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-18 13:01:59 -0300

    feat: build mixed spell card print document

--- src/printJobs/spellCardsJob.js
+++ src/printJobs/spellCardsJob.js
@@ -1,16 +1,19 @@
 import { sampleSpells } from "../data/sampleSpells.js";
-import { createPrintDocument } from "../core/print/createPrintDocument.js";
+import { createMixedPrintDocument } from "../core/print/createMixedPrintDocument.js";
+import { createSpellCardPrintRecords } from "../templates/spellCard/createSpellCardPrintRecords.js";
+import { spellCardFlowRegions } from "../templates/spellCard/flowRegions.js";
 import { createSpellCardTemplateContext } from "../templates/spellCard/resolveSpellCardTheme.js";
 import { getSpellCardVariant } from "../templates/spellCard/variants.js";
 
 export function createSpellCardsJob({ variantId = "classic" } = {}) {
   const variant = getSpellCardVariant(variantId);
   const templateData = sampleSpells.map(createSpellCardTemplateContext);
-  const printDocument = createPrintDocument({
-    data: templateData,
-    manifest: variant.manifest,
-    templateHtml: variant.templateHtml,
-    templateStyles: variant.templateStyles,
+  const printRecords = createSpellCardPrintRecords(templateData, spellCardFlowRegions, {
+    baseVariantId: variant.id,
+  });
+  const printDocument = createMixedPrintDocument({
+    records: printRecords,
+    resolveVariant: getSpellCardVariant,
   });
 
   return {
@@ -19,6 +22,7 @@ export function createSpellCardsJob({ variantId = "classic" } = {}) {
     variantId: variant.id,
     variantLabel: variant.label,
     data: templateData,
+    printRecords,
     manifest: variant.manifest,
     templateHtml: variant.templateHtml,
     templateStyles: variant.templateStyles,
