196a9c505181bb8d423df1666f80d5fbe0513e31
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-06-18 10:32:57 -0300

    feat: render data template and preview tabs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createPrintJobInspector } from "./app/createPrintJobInspector.js";
+import { createAppTabs } from "./app/createAppTabs.js";
+import { createDataView } from "./app/createDataView.js";
+import { createPreviewInfoView } from "./app/createPreviewInfoView.js";
+import { createTemplateView } from "./app/createTemplateView.js";
 import { createSpellCardsJob } from "./printJobs/spellCardsJob.js";
 import { measurePrintBlockOverflow } from "./render/measurePrintBlockOverflow.js";
 import { renderPrintDocument } from "./render/renderPrintDocument.js";
 
-const inspectorTarget = document.querySelector("#printJobInspector");
+const appViewsTarget = document.querySelector("#appViews");
+const previewInfoTarget = document.createElement("div");
 const previewTarget = document.querySelector("#printPreview");
 const printButton = document.querySelector("#printButton");
 
@@ -11,7 +15,31 @@ const spellCardsJob = createSpellCardsJob();
 
 renderPrintDocument(spellCardsJob.printDocument, previewTarget);
 const overflowReport = measurePrintBlockOverflow(previewTarget);
-inspectorTarget.append(createPrintJobInspector(spellCardsJob, { overflowReport }));
+
+previewInfoTarget.append(createPreviewInfoView(spellCardsJob, { overflowReport }));
+
+appViewsTarget.append(
+  createAppTabs(
+    [
+      {
+        id: "data",
+        label: "Data",
+        content: createDataView(spellCardsJob),
+      },
+      {
+        id: "template",
+        label: "Template",
+        content: createTemplateView(spellCardsJob),
+      },
+      {
+        id: "preview",
+        label: "Preview",
+        content: previewInfoTarget,
+      },
+    ],
+    "preview",
+  ),
+);
 
 printButton.addEventListener("click", () => {
   window.print();
